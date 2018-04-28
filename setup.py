d67914ebf9282e3256d061280e02844526ef1108
Jamie Salter <jamie.a.salter@googlemail.com>
2018-04-28 18:14:33 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lewis',
+    version='',
+    install_requires=[
+        "gym"
+    ]
+    packages=['lewis'],
+    url='',
+    license='',
+    author='jamiesalter',
+    author_email='',
+    description=''
+)
